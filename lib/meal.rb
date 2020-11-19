752124990573032539a67d2ac02c07df0edf79bb
Zach <zbayard@gmail.com>
2020-11-19 17:40:18 -0500

    finished

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,25 @@
 class Meal
 
+    attr_accessor :waiter, :customer, :total, :tip
+
+
+    @@all = []
+
+    def initialize(waiter, customer, total, tip)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    
+
+
+
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,39 @@
 class Waiter
 
+    attr_accessor :name, :experience
+
+    @@all = []
+
+    def initialize(name, experience)
+        @name = name
+        @experience = experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+    def best_tipper
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+            meal_a.tip <=> meal_b.tip
+        end
+
+        best_tipped_meal.customer
+    end
+    
+
+
+
+
 end
